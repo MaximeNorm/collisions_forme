1fa35282eced55d61b98e62cd17132aee84c19f0
MaximeNorm <maxime-n-f@hotmail.fr>
2023-03-14 13:26:47 -0400

    test

--- js/main.js
+++ js/main.js
@@ -3,20 +3,21 @@ function test_collision_rectangles(rect1, rect2) {
         rect1.x + rect1.width > rect2.x &&
         rect1.y < rect2.y + rect2.height &&
         rect1.height + rect1.y > rect2.y) {
-         return true
-        }   
+        return true
     }
-    
-    
+}
+
+
 function test_collision_cercles(cercle1, cercle2) {
     // Calculate the distance between the centers of the circles
-let dx = cercle1.x - cercle2.x;
-let dy = cercle1.y - cercle2.y;
-let distance = Math.sqrt(dx * dx + dy * dy);
+    let dx = cercle1.x - cercle2.x;
+    let dy = cercle1.y - cercle2.y;
+    let distance = Math.sqrt(dx * dx + dy * dy);
 
-// Check if the circles are colliding
-if (distance < cercle1.radius + cercle2.radius) {
+    // Check if the circles are colliding
+    if (distance < cercle1.radius + cercle2.radius) {
 
-    return true
-}
+
+        return true
+    }
 }
